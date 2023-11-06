d6d4c089cea37e74d7f0b16a231b0cf101aadb6f
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-11-06 11:00:53 +0700

    guard pubilc

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,16 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { AuthGuard } from './common/guard/auth.guard';
 
 @Module({
   imports: [PrismaModule, AuthModule],
   controllers: [],
-  providers: [],
+  providers: [{
+    provide: APP_GUARD,
+    useClass: AuthGuard
+  }],
 })
 
 export class AppModule { }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,17 +2,20 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Auth } from './type';
 import { AuthDto } from './dto';
+import { Public } from 'src/common/decorators/pubilc.decorator';
 
 @Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) { }
 
+    @Public()
     @Post('local/register')
     @HttpCode(HttpStatus.CREATED)
     registerLoacl(@Body() dto: AuthDto): Promise<Auth> {
         return this.authService.registerLocal(dto)
     }
 
+    @Public()
     @HttpCode(HttpStatus.OK)
     @Post('local/login')
     loginLocal(@Body() dto: AuthDto) {
