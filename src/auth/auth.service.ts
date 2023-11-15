06109b607af35db21e1d170035f1952d66e7511f
Tao-Toxicboy19 <ppv_tao@outlook.co.th>
2023-11-16 01:08:14 +0700

    upload file

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -44,7 +44,7 @@ export class AuthService {
             throw new UnauthorizedException('Invalid credentials');
         }
 
-        const payload = { sub: user.id, username: user.username };
+        const payload = { sub: user.id, username: user.username, profile: user.profile };
         const tokens = { access_token: await this.jwtService.signAsync(payload) }
 
         return tokens
